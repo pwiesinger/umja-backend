0777f76b728433787a6f8b0774db6396be98d2a8
Paul Wiesinger <paul.n.w@icloud.com>
2020-01-06 21:57:11 +0100

    Methods parse is finished.

--- src/main/java/com/lab73/model/Attribute.java
+++ src/main/java/com/lab73/model/Attribute.java
@@ -1,9 +1,45 @@
 package com.lab73.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Attribute {
     private String accessLevel;
     private boolean isStatic;
     private boolean isFinal;
     private String type;
     private String name;
+
+    public Attribute(String accessLevel, boolean isStatic, boolean isFinal, String type, String name) {
+        this.accessLevel = accessLevel;
+        this.isStatic = isStatic;
+        this.isFinal = isFinal;
+        this.type = type;
+        this.name = name;
+    }
+
+    public static List<Attribute> parseMethods(String raw) {
+        if (raw.contains("<html>")) {
+            return parseHTML(raw);
+        } else {
+            return parseRegular(raw);
+        }
+    }
+
+    private static List<Attribute> parseHTML(String raw) {
+        return null;
+    }
+
+    private static List<Attribute> parseRegular(String raw) {
+        List<Attribute> attributes = new ArrayList<>();
+
+        String[] lines = raw.split("\n");
+        for (String line : lines) {
+            line = line.trim();
+            boolean isPublic = line.startsWith("+");
+            line.
+        }
+
+        return null;
+    }
 }
