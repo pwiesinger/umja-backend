600163192e40ca7b3e9c8fc9a1b15c5b437cd76e
davidgangl <david.gangl@lab73.at>
2020-01-06 23:23:19 +0100

    current state (compiler)

--- src/main/java/com/lab73/logic/Compiler.java
+++ src/main/java/com/lab73/logic/Compiler.java
@@ -1,5 +1,6 @@
 package com.lab73.logic;
 
+import com.lab73.model.ClassFile;
 import com.lab73.model.Project;
 
 import java.io.File;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Compiler {
 
@@ -17,9 +19,6 @@ public class Compiler {
     }
 
     public void writeOutFiles() {
-
-
-
         for (String packageName : project.getPackages().keySet())  {
             File theDir = new File("/Users/davidgangl/Desktop/AUD/src/main/java/com/lab73/testProject/" + packageName);
 
@@ -38,6 +37,9 @@ public class Compiler {
                     System.out.println("DIR created");
                 }
             }
+
+
+            ArrayList<ClassFile> classFile = project.getPackages().get(packageName);
         }
     }
 
