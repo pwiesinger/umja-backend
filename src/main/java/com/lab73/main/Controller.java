8d6381fd2c07549b11001794ab7a98f71bb9f7b8
Paul Wiesinger <paul.n.w@icloud.com>
2020-01-06 17:22:42 +0100

    added dummy interface

--- src/main/java/com/lab73/main/Controller.java
+++ src/main/java/com/lab73/main/Controller.java
@@ -1,12 +1,25 @@
 package com.lab73.main;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 @RestController
 public class Controller {
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World!";
+
+    @RequestMapping("/convert")
+    public ResponseEntity<Resource> convert(@RequestParam("file") MultipartFile file) throws MalformedURLException {
+        Resource resource = new UrlResource(new File("text.txt").toURI());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
