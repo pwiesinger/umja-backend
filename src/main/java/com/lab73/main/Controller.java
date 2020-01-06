0b0a7f41edd7e1e477bf0a413fbfffec732ed896
paul.krenn <paul.krenn@lab73.at>
2020-01-06 22:37:15 +0100

    Implemented the option to upload raw text!

--- src/main/java/com/lab73/main/Controller.java
+++ src/main/java/com/lab73/main/Controller.java
@@ -1,14 +1,14 @@
 package com.lab73.main;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -17,6 +17,7 @@ import java.net.MalformedURLException;
 @RestController
 public class Controller {
 
+    Logger logger = LoggerFactory.getLogger(Controller.class);
     @PostMapping("/convert")
     public ResponseEntity<Resource> convert(@RequestParam("file") MultipartFile file) throws MalformedURLException {
         Resource resource = new UrlResource(new File("text.txt.zip").toURI());
@@ -25,4 +26,15 @@ public class Controller {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
+
+    @RequestMapping(value = "/convertText", method = RequestMethod.POST)
+    public ResponseEntity<Resource> Test(@RequestBody String input) throws MalformedURLException {
+        Resource resource = new UrlResource(new File("text.txt.zip").toURI());
+        logger.info(input);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
 }
