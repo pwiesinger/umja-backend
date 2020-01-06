74f63c1f6fd23f15b02735246a6ccd8977310992
Paul Wiesinger <paul.n.w@icloud.com>
2020-01-06 21:00:01 +0100

    changes

--- src/main/java/com/lab73/logic/Compiler.java
+++ src/main/java/com/lab73/logic/Compiler.java
@@ -1,4 +1,24 @@
 package com.lab73.logic;
 
+import com.lab73.model.Project;
+
 public class Compiler {
+
+    private Project project;
+
+    public Compiler(Project project) {
+        this.project = project;
+    }
+
+    public void writeOutFiles() {
+
+    }
+
+    public void tryToCompile() {
+
+    }
+
+    public void generateZip() {
+
+    }
 }
