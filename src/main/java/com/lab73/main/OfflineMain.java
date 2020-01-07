a5547c819db15d23c1d285d0c8d5e5cf72cb6819
Paul Wiesinger <paul.n.w@icloud.com>
2020-01-07 02:23:25 +0100

    xyvb

--- src/main/java/com/lab73/main/OfflineMain.java
+++ src/main/java/com/lab73/main/OfflineMain.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class OfflineMain {
-    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+    /*public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
         File file = new File("sample/uml_v3.graphml");
         Parser parser = new Parser(file);
         Project project = parser.parse();
@@ -19,5 +19,5 @@ public class OfflineMain {
         compiler.writeOutFiles();
         //compiler.tryToCompile();
         compiler.generateZip();
-    }
+    }*/
 }
